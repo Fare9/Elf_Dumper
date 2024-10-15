1ec13b06b7c0b804a834e1525e6d3927ee5c9d9e
Fare9 <eduardo.blazquez@uc3m.es>
2024-10-15 23:19:46 +0200

    Added comments

--- headers/logger.h
+++ headers/logger.h
@@ -3,11 +3,29 @@
 
 #include "headers.h"
 
+/**
+ * Method to print error messages.
+ */
 void log_error(const char* message, ...);
+
+/**
+ * Method to print info messages.
+ */
 void log_info(const char* message, ...);
+
+/**
+ * Method to print debug messages.
+ */
 void log_debug(const char* message, ...);
 
+/**
+ * Set a global error flag to show error messages.
+ */
 void set_error_flag();
+
+/**
+ * Set a global debug flag to show debug messages.
+ */
 void set_debug_flag();
 
 #endif
--- headers/memory_management.h
+++ headers/memory_management.h
@@ -3,13 +3,59 @@
 
 #include "headers.h"
 
+/**
+ * Allocate memory using malloc with a specific size.
+ * @param size bytes to allocate
+ * @return pointer to allocated memory
+ */
 void* allocate_memory(size_t size);
+
+/**
+ * Reallocate a memory previously allocated
+ * using realloc.
+ * @param ptr memory previously allocated
+ * @param size new size to allocate
+ * @return new pointer with new memory
+ */
 void* realloc_memory(void* ptr, size_t size);
+
+/**
+ * Mmap a file descriptor with read permissions only.
+ * @param length length of the file to map into memory
+ * @param fd descriptor of the fiel to map into memory
+ * @return pointer to the mapped file
+ */
 void* mmap_file_read(size_t length, int fd);
+
+/**
+ * Mmap a file descriptor with write permissions only.
+ * @param length length of the file to map into memory
+ * @param fd descriptor of the fiel to map into memory
+ * @return pointer to the mapped file
+ */
 void* mmap_file_write(size_t length, int fd);
+
+/**
+ * Mmap a file descriptor with read and write permissions.
+ * @param length length of the file to map into memory
+ * @param fd descriptor of the fiel to map into memory
+ * @return pointer to the mapped file
+ */
 void* mmap_file_read_write(size_t length, int fd);
 
+/**
+ * Free allocated memory.
+ * @param ptr pointer to allocated memory
+ * @return error code
+ */
 int free_memory(void *ptr);
+
+/**
+ * Unmap mapped memory.
+ * @param ptr pointer to mapped memory
+ * @param size size of mapped memory
+ * @return error code
+ */
 int munmap_memory(void* ptr, size_t size);
 
 #endif
